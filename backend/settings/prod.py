a5eba06c2578fd36e7277dd35a4f94e9962fe07c
R3Z <rogerjohnmorellizeller@gmail.com>
2024-10-02 23:53:45 -0400

    Updated requirements.txt

--- backend/settings/prod.py
+++ backend/settings/prod.py
@@ -9,8 +9,4 @@ DEBUG = True
 print("Looking for production database.")
 DB_URL = os.getenv("DATABASE_URL")
 print(f"Found DB_URL: {DB_URL}")
-DATABASES["default"] = dj_database_url.config(
-    conn_max_age=600, ssl_require=True, default=DB_URL
-)
-
-SECRET_KEY = "rb@&a^^7$7#f7c*e*^+55&sknu6g!f+2zuh09bagm2#veitcg0"
+DATABASES["default"] = dj_database_url.config(conn_max_age=600, ssl_require=True)
