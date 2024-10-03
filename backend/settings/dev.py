119245019fee81ba15515c4f67f95f542510f2e2
R3Z <rogerjohnmorellizeller@gmail.com>
2024-10-02 22:59:12 -0400

    changed allowed hosts for heroku test

--- backend/settings/dev.py
+++ backend/settings/dev.py
@@ -31,7 +31,9 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = json.loads(os.getenv("ALLOWED_HOSTS"))
+# ALLOWED_HOSTS = json.loads(os.getenv("ALLOWED_HOSTS"))
+
+ALLOWED_HOSTS = ["https://git.heroku.com/vdrf-template.git"]
 
 # Application definition
 INSTALLED_APPS = [
