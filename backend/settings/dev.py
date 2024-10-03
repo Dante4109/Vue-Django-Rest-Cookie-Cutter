4ccd9b3368abdd041fc17f4d1307f23552ccf80b
R3Z <rogerjohnmorellizeller@gmail.com>
2024-10-02 23:21:26 -0400

    Updated requirements.txt

--- backend/settings/dev.py
+++ backend/settings/dev.py
@@ -26,7 +26,8 @@ load_dotenv(os.path.join(str(SETTINGS_DIR), ".env"))
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+# SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+SECRET_KEY = "p)r(%z$btd3bz%so29v$!$s0j9)ld9)ybq@r9nolo(t85j2!wk"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
