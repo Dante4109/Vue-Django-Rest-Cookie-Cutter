56430e241df7c0875823122581333dbfa87e0cf5
R3Z <rogerjohnmorellizeller@gmail.com>
2024-10-04 16:36:27 -0400

    deleted frontend folder

--- frontend/vuetify/src/EventBus.ts
+++
@@ -1,5 +0,0 @@
-import mitt from "mitt";
-export default mitt();
-
-// import { useEventBus } from "@vueuse/core";
-// export default useEventBus<string>("EVENT");
--- frontend/vuetify/src/composables/accessEmitter.js
+++
@@ -1,5 +0,0 @@
-import { getCurrentInstance } from 'vue';
-
-export default function accessEmitter() {
-    return getCurrentInstance().appContext.config.globalProperties.emitter;
-}
